d3bab3d72f7d02328c383e84c49306862d512f57
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2017-01-12 12:28:50 +0400

    Create custom i18n\Formatter class

---
+++ component/i18n/Formatter.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * User: zura
+ * Date: 1/12/17
+ * Time: 12:23 PM
+ */
+
+namespace centigen\base\component\i18n;
+
+use yii\bootstrap\Html;
+
+
+/**
+ * Class Formatter
+ *
+ * @author Zura Sekhniashvili <zurasekhniashvili@gmail.com>
+ * @package omcrn\base\component\i18n
+ */
+class Formatter extends \yii\i18n\Formatter
+{
+    public function asStatusLabel($status)
+    {
+        if ($status){
+            return Html::tag('span', \Yii::t('omcrnActive', "Active"), ['label label-success']);
+        }
+        return Html::tag('span', \Yii::t('omcrnActive', "Inactive"), ['label label-danger']);
+    }
+}

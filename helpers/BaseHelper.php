ee65289462c91ed0cea309f56bf17a5f89b6aeb6
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2017-01-27 13:07:24 +0400

    Remove getAvailableLocales method from BaseHelper

--- helpers/BaseHelper.php
+++ helpers/BaseHelper.php
@@ -1,34 +1,14 @@
 <?php
 
 namespace centigen\base\helpers;
-use Yii;
 
 /**
  * Class BaseHelper
  *
- * @author zura
- * @package centigen\i18ncontent\helpers
+ * @author Zura Sekhniashvili <zurasekhniashvili@gmail.com>
+ * @package centigen\base\helpers
  */
 class BaseHelper 
 {
-    /**
-     * Returns availableLocales from `Yii::$app->params` if it is set. If not it will return Yii::$app->language
-     *
-     * @author Zura Sekhniashvili <zurasekhniashvili@gmail.com>
-     * @return array
-     * @deprecated Will be removed on v2.0.0. Use LocaleHelper::getAvailableLocales instead
-     */
-    public static function getAvailableLocales()
-    {
-        $locales = [];
-        if (isset(Yii::$app->params['availableLocales'])) {
-            $locales = Yii::$app->params['availableLocales'];
-        }
 
-        if (count($locales) === 0) {
-            $locales = [Yii::$app->language];
-        }
-
-        return $locales;
-    }
 } 
